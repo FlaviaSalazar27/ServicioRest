30a6f3f2f1b34ba7c9a4c3f50a1f3c7c50c2b132
FlaviaSalazar27 <flaviasalazarovalle@gmail.com>
2019-09-12 16:57:23 -0400

    creando interface

---
+++ AnfitrionPuma/src/main/java/com/Puma/AnfitrionPuma/RepositorioAnfitrion.java
@@ -0,0 +1,6 @@
+package com.Puma.AnfitrionPuma;
+ import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RepositorioAnfitrion  extends JpaRepository<AnfitrionP, Long> {
+
+}
