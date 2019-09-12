1ac517447f72a18e445edf570e95ebf371467e9a
FlaviaSalazar27 <flaviasalazarovalle@gmail.com>
2019-09-12 16:51:40 -0400

    creando clase AnfitrionP

--- AnfitrionPuma/src/main/java/com/Puma/AnfitrionPuma/AnfitrionP.java
+++ AnfitrionPuma/src/main/java/com/Puma/AnfitrionPuma/AnfitrionP.java
@@ -1,4 +1,28 @@
 package com.Puma.AnfitrionPuma;
+import lombok.Data;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Data
+@Entity
 public class AnfitrionP {
+    private @Id @GeneratedValue Long id;
+    private String nombre;
+    private String email;
+    private int telefono;
+    private String ci;
+
+
+    AnfitrionP() {}
+
+    AnfitrionP(String nombre, String email, int telefono, String ci) {
+        this.nombre = nombre;
+        this.email = email;
+        this.telefono = telefono;
+        this.ci = ci;
+
+    }
+
 }
